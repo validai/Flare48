570813470d2a33de5489b385fdf6f5a24fa7488b
jaidcoding <jaidcoding@gmail.com>
2025-02-18 14:33:24 -0600

    Update client package.json and vite.config.js for improved development setup and add proxy configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Ensures correct asset paths in production
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000", // Use only for local development
+    }
+  }
 })
