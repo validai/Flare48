82a4dc211307a929a1d9a8c001eaa1f2958127c7
jaidcoding <jaidcoding@gmail.com>
2025-02-17 18:47:46 -0600

    Fix formatting in vite.config.js and update package-lock.json for dev dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,4 +8,4 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-})
+});
