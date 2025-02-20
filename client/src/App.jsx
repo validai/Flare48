6c632ec9910673f184287df89d6ac8f1e66a1b71
validai <djcoast239@gmail.com>
2025-02-20 17:53:14 -0500

    Started implementing news APIs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar';
+import SearchBox from './components/SearchBox';
 
 function App() {
   return (
     <>
       <Navbar />
+      <SearchBox />
     </>
   );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import apiRoutes from './routes/api.js';
 
 // Load environment variables
 dotenv.config();
