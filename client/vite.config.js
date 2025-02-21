3c2235cbf0afb1233126be8668ba7c9515feaa32
validai <djcoast239@gmail.com>
2025-02-21 16:32:20 -0500

    Removed dotenv from vite.config.js to fix deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
+import * as dotenv from 'dotenv'
 import tailwindcss from '@tailwindcss/vite'
+dotenv.config();
 
 
 // https://vite.dev/config/
