744cc4b415a7e5b3855fd9036d7db61cb12b7c7a
Daniel V <d.villa.exe@gmail.com>
2025-03-08 13:58:36 -0500

    fixed code for cors

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware Setup
 console.log("Initializing Middleware...");
 app.use(cors({ origin: process.env.CLIENT_URL }));
-app.use(cors({ origin: process.env.VITE_FRONTEND_URL, credentials: true }));v
+app.use(cors({ origin: process.env.VITE_FRONTEND_URL, credentials: true }));
 console.log("✅ CORS Enabled");
 app.use(express.json());
 console.log("✅ JSON Middleware Enabled");
